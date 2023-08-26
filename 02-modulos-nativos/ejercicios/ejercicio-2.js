9f4fb26fba1b7fb001bd47f8bcbd1e409e519492
jos30spina <125369495+jos30spina@users.noreply.github.com>
2023-08-26 10:07:38 -0500

    Update ejercicio-2.js
    se cambio el archivo 02

--- 02-modulos-nativos/ejercicios/ejercicio-2.js
+++ 02-modulos-nativos/ejercicios/ejercicio-2.js
@@ -17,21 +17,3 @@ Adios el proceso ha terminado!
 
 */
 
-// Obtener el argumento de línea de comandos
-const nombre = process.argv[2];
-
-// Mostrar el mensaje de saludo
-console.log(`Hola ${nombre}`);
-
-// Obtener el valor de la variable de entorno NODE_ENV
-const nodeEnv = process.env.NODE_ENV;
-
-// Mostrar el mensaje según el valor de NODE_ENV
-if (nodeEnv === "development") {
-  console.log("Estamos en desarrollo");
-} else if (nodeEnv === "production") {
-  console.log("Estamos en producción");
-}
-
-// Mostrar el mensaje de despedida
-console.log("Adios el proceso ha terminado!");
